4338b4f802fb5103141cf2776582c6031d90a749
Andrii <krenevych@knu.ua>
2021-05-18 10:19:29 +0300

    Алгоритм Дейкстри (ще потрібно відновити шлях)

--- labs/L25/task2/user.py
+++ labs/L25/task2/user.py
@@ -1,8 +1,23 @@
 """
 Нехай задано орієнтований зважений граф.
-Знайдіть довжину найкоротшого шляху між двома заданими вузлами цього графа
+Знайдіть найкоротший шляху між двома заданими вузлами цього графа
 """
+from labs.L25.task2.PriorityQueue import PriorityQueue
 
+INF = 100000000
+
+
+class Vertex:
+    def __init__(self, key):
+        self.key = key
+        self.neighbour = {}  # список (словник ключ_вершини: вага ребра) сусідів
+        self.distance = INF
+
+    def __str__(self):
+        return str(self.key) + ":" + str(*self.neighbour.keys())
+
+
+graph = []  # граф, як список вершин
 
 def init(vertices, edges):
     """ Ініціалізація графа.
@@ -11,7 +26,9 @@ def init(vertices, edges):
     @param vertices: кількість вершин графа
     @param edges:  кількість ребер графа
     """
-    pass
+    global graph
+    for i in range(vertices):
+        graph.append(Vertex(i))
 
 
 def addEdge(source, destination, weight):
@@ -21,7 +38,8 @@ def addEdge(source, destination, weight):
     @param destination: вершина у яку входить ребро
     @param weight: вага ребра
     """
-    pass
+    global graph
+    graph[source].neighbour[destination] = weight
 
 
 def getWay(start, end):
@@ -31,5 +49,29 @@ def getWay(start, end):
     @param end: кінцева вершина
     @return: список вершин шляху або порожній список, якщо шляху між вершинами не існує.
     """
+    global graph
+    for v in graph:
+        v.distance = INF
 
+    graph[start].distance = 0
+    pq = PriorityQueue()
+    for key_ver in range(len(graph)):
+        pq.insert(key_ver, graph[key_ver].distance)
+
+    fixed = set()  # список фіксованих(оброблених) вершин
+
+    while not pq.empty():
+        vert = pq.extractMinimum()
+        fixed.add(vert)
+
+        for neigb, weight in graph[vert].neighbour.items():
+            if neigb in fixed:
+                continue
+
+            new_dist = graph[vert].distance + weight
+            if new_dist < graph[neigb].distance:
+                graph[neigb].distance = new_dist
+                pq.updatePriority(neigb, graph[neigb].distance)
+
+    dist = graph[end].distance
     return None
