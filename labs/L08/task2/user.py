25f9eac746f0e4071d4469583b087cd4a5699601
Andrii <krenevych@knu.ua>
2021-06-03 19:09:58 +0300

    Очищено функції

--- labs/L08/task2/user.py
+++ labs/L08/task2/user.py
@@ -17,11 +17,7 @@ def sort(array):
     """ Сортування масиву
     :param array: Вхідний масив даних, що треба відсортувати.
     """
-    n = len(array)
-    for pass_num in range(n - 1, 0, -1):
-        for i in range(pass_num):
-            if array[i] > array[i + 1]:
-                array[i], array[i + 1] = array[i + 1], array[i]
+    pass
 
 
 
--- labs/L09/task3/user.py
+++ labs/L09/task3/user.py
@@ -5,7 +5,7 @@
 масив відсортований за спаданням елементів).
 """
 
-N = 1000     # Кількість елементів масиву.
+N = 10000     # Кількість елементів масиву.
               # Використовується у головній програмі для генерування
               # масиву з випадкових чисел
 
@@ -15,26 +15,15 @@ def bubble_sort(array):
 
     :param array: Масив (список однотипових елементів)
     """
-    n = len(array)
-    for pasNum in range(n - 1, 0, -1):
-        for i in range(pasNum):
-            if array[i] > array[i + 1]:
-                array[i], array[i + 1] = array[i + 1], array[i]
+    pass
+
 
 def bubble_sort_optimized(array):
     """ Модификований алгоритм сортування "Бульбашкою"
 
     :param array: Вхідний масив даних, що треба відсортувати.
     """
-    n = len(array)
-    for pass_num in range(n - 1, 0, -1):
-        isSorted = True
-        for i in range(pass_num):
-            if array[i] > array[i + 1]:
-                isSorted = False
-                array[i], array[i + 1] = array[i + 1], array[i]
-        if isSorted:
-            break
+    pass
 
 
 def selection_sort(array):
@@ -43,14 +32,7 @@ def selection_sort(array):
     :param array: Масив (список однотипових елементів)
     :return: None
     """
-    n = len(array)
-    for pass_num in range(n - 1, 0, -1):
-        ind_max = 0
-        for i in range(1, pass_num + 1):
-            if array[i] > array[ind_max]:
-                ind_max = i
-
-        array[ind_max], array[pass_num] = array[pass_num], array[ind_max]
+    pass
 
 
 def insertion_sort(array):
@@ -59,15 +41,7 @@ def insertion_sort(array):
     :param array: Масив (список однотипових елементів)
     :return: None
     """
-    n = len(array)
-    for index in range(1, n):
-        current = array[index]
-        for i in range(index - 1, -1, -1):
-            if array[i] > current:
-                array[i + 1] = array[i]
-            else:
-                array[i + 1] = current
-                break
+    pass
 
 
 def merge_sort(array):
@@ -76,40 +50,7 @@ def merge_sort(array):
     :param array: Масив (список однотипових елементів)
     :return: None
     """
-    if len(array) <= 1:
-        return
-        # 2. розбиваємо масив на дві половини і здійснюємо
-        #    рекурсивний вклик сортування для кожної половини.
-    m = len(array) // 2
-    left = array[:m]
-    right = array[m:]
-    merge_sort(left)
-    merge_sort(right)
-    # 3. зливаємо дві відсортовані половини у один
-    #    відсортований масив
-    i = 0
-    j = 0
-    k = 0
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            array[k] = left[i]
-            i += 1
-        else:
-            array[k] = right[j]
-            j += 1
-        k += 1
-
-    while i < len(left):  # якщо залишилися елементи
-        # у лівому масиві
-        array[k] = left[i]
-        i += 1
-        k += 1
-
-    while j < len(right):  # якщо залишилися елементи
-        # у правому масиві
-        array[k] = right[j]
-        j += 1
-        k += 1
+    pass
 
 
 def quick_sort(array):
@@ -118,38 +59,5 @@ def quick_sort(array):
         :param array: Масив (список однотипових елементів)
         :return: None
         """
-    qsort(array, 0, len(array) - 1)
-
-
-def qsort(array, a, b):
-    # 1. Тривіальний випадок, коли [a, b] містить
-    # лише один елемент, або порожній
-    if a >= b:
-        return
-
-    # pivot = array[a + (b - a) // 2]  # опорний елемент
-    pivot = array[a]  # опорний елемент
-    # 2. Розбиваємо вхідний масив на дві частини так,
-    # щоб ліва частина містили елементи масиву,
-    # що менші або рівні за опорний,
-    # а права - більші або рівні за опорний
-    left = a
-    right = b
-
-    while True:
-        while array[left] < pivot:
-            left += 1
-
-        while array[right] > pivot:
-            right -= 1
-
-        if left >= right:
-            break
-
-        array[left], array[right] = array[right], array[left]
-        left += 1
-        right -= 1
-
-    qsort(array, a, right)
-    qsort(array, right + 1, b)
+    pass
 
