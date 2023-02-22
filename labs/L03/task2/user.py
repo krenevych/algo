4418815b930d6501838d48d61b0fe5ab4cd535b9
Andrii Krenevych <akrenevych@gmail.com>
2023-02-22 10:19:35 +0200

    розв'язок 3.2

--- labs/L03/task2/user.py
+++ labs/L03/task2/user.py
@@ -2,6 +2,8 @@
 Реалізуйте інтерфейс для роботи з англійсько-українським словником та швидким пошуком перекладу.
 """
 
+en_words = []
+ua_words = []
 
 def addTranslation(eng, translation):
     """ Додає до словника англійське слово та його переклад.
@@ -10,7 +12,9 @@ def addTranslation(eng, translation):
     :param eng: англійське слово
     :param translation: переклад
     """
-    pass
+    en_words.append(eng)
+    ua_words.append(translation)
+
 
 
 def find(eng):
@@ -18,4 +22,21 @@ def find(eng):
     :param eng: англійське слово
     :return: переклад слова, якщо воно міститься у словнику, або порожній рядок у іншому разі.
     """
-    return ""
+    left = 0                # Індекс лівого елементу
+    right = len(en_words) - 1  # Індекс правого елементу
+
+    while left < right:
+        m = left + (right - left) // 2  # Індекс середнього елементу
+        if eng > en_words[m]:
+            left = m + 1
+        else:
+            right = m
+
+    found = en_words[right]
+
+    if found == eng:
+        return ua_words[right]
+    else:
+        return ""
+
+
