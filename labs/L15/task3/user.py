e0c9ca071427dcf97c1cdb54c550a08f7bbd4871
Andrii Krenevych <akrenevych@gmail.com>
2023-02-24 11:52:09 +0200

    умова задачі параграфу 15

--- labs/L15/task3/user.py
+++ labs/L15/task3/user.py
@@ -1,27 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class Node:
-    def __init__(self, item):
-        self.item = item
-        self.next = None
-        self.prev = None
-
-    def __str__(self):
-        return str(self.item)
-
-"""
-Реалізуйте структуру даних двобічнозв'язний список з поточним елементом.
-"""
-
-front : Node = None
-last  : Node = None
-curr  : Node = None
-
 def init():
     """ Викликається один раз на початку виконання програми. """
-    global front, last, curr
-    front =  last = curr = None
+    pass
 
 
 def empty():
@@ -29,8 +11,7 @@ def empty():
 
     :return: True, якщо список не містить жодного елемента
     """
-    global front, last, curr
-    return curr is None
+    return True
 
 
 def set_first():
@@ -39,8 +20,8 @@ def set_first():
     Переставляє поточний елемент на перший елемент списку.
     Гарантується, що функція не буде викликана, якщо список порожній.
     """
-    global front, last, curr
-    curr = front
+    pass
+
 
 def set_last():
     """ Робить останній елемент списку, поточним
@@ -48,8 +29,7 @@ def set_last():
     Переставляє поточний елемент на останній елемент списку
     Гарантується, що функція не буде викликана, якщо список порожній.
     """
-    global front, last, curr
-    curr = last
+    pass
 
 
 def next():
@@ -58,11 +38,7 @@ def next():
     Робить поточним елементом списку, елемент що йде за поточним.
     Породжує виключення StopIteration, якщо поточний елемент є останнім у списку.
     """
-    global front, last, curr
-    if curr == last:
-        raise StopIteration
-
-    curr = curr.next
+    pass
 
 
 def prev():
@@ -71,11 +47,7 @@ def prev():
     робить поточним елементом елемент списку, що йде перед поточним.
     Породжує виключення StopIteration, якщо поточний елемент є першим у списку.
     """
-    global front, last, curr
-    if curr == front:
-        raise StopIteration
-
-    curr = curr.prev
+    pass
 
 
 def current():
@@ -84,8 +56,7 @@ def current():
     Гарантується, що функція не буде викликана, якщо список порожній.
     :return: Навантаження поточного елементу
     """
-    global front, last, curr
-    return curr.item
+    return None
 
 
 def insert_after(item):
@@ -93,40 +64,15 @@ def insert_after(item):
 
     :param item: елемент, що вставляється у список
     """
-    global front, last, curr
-    node = Node(item)
-    if empty():
-        front = last = curr = node
-        return
+    pass
 
-    node.prev = curr
-    if curr == last:
-        last = node
-    else:
-        curr.next.prev = node
-        node.next = curr.next
-
-    curr.next = node
 
 def insert_before(item):
     """ Вставляє новий елемент у список перед поточним.
 
     :param item: елемент, що вставляється у список
     """
-    global front, last, curr
-    node = Node(item)
-    if empty():
-        front = last = curr = node
-        return
-
-    node.next = curr
-    if curr == front:
-        front = node
-    else:
-        curr.prev.next = node
-        node.prev = curr.prev
-
-    curr.prev = node
+    pass
 
 
 def delete():
@@ -136,18 +82,4 @@ def delete():
     Якщо елемент, що видаляється був у списку останнім, то поточним стає передостанній елемент цього списку.
     Гарантується, що функція не буде викликана, якщо список порожній.
     """
-    global front, last, curr
-    if front == last:
-        front = last = curr = None
-        return
-
-    if curr.prev != None:
-        curr.prev.next = curr.next
-
-    if curr.next != None:
-        curr.next.prev = curr.prev
-        curr = curr.next
-    else:
-        curr = curr.prev
-
-
+    pass
