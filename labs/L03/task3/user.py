6047d536a2991d5bad2ae2a2e1454a0086d103d9
Andrii Krenevych <akrenevych@gmail.com>
2023-02-24 09:27:45 +0200

    Solution 3.3

--- labs/L03/task3/user.py
+++ labs/L03/task3/user.py
@@ -12,5 +12,28 @@ def bsearch_leftmost(array, x):
     :param x:     Шукане число
     :return:      Номер шуканого елемента у масиві
     """
-    # TODO: реалізуйте алгоритм тут
-    return 0
+    left = 0            # ліва границя пошуку
+    right = len(array)  # права границя пошуку
+    while left < right:
+        m = left + (right - left) // 2  # Середина
+        if array[m] < x:
+            left = m + 1
+        else:
+            right = m
+
+    return left
+    # return 0
+
+
+if __name__ == '__main__':
+    myarray = [
+        1, 1, 1, 1, 1, 4, 4, 4, 4, 4,
+        5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+        5, 5, 5, 5, 5, 9, 9, 9
+    ]
+
+    res = bsearch_leftmost(myarray, 5)
+    res = bsearch_leftmost(myarray, 3)
+    res = bsearch_leftmost(myarray, 90)
+    res = bsearch_leftmost(myarray, 0)
+    print(res)
