4a79c26e01981716110296b1b2988bd0290c7ce3
Andrii Krenevych <akrenevych@gmail.com>
2023-04-19 10:58:30 +0300

    Solution lab 15

--- labs/L15/task1/user.py
+++ labs/L15/task1/user.py
@@ -5,10 +5,74 @@
 Реалізуйте структуру даних зв'язний (однозв'язний) список з поточним елементом.
 """
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class ClassicList:
+    def __init__(self):
+        """ Викликається один раз на початку виконання програми. """
+        self.first = None
+        self.curr = None
+
+
+    def empty(self):
+        """ Перевіряє чи список порожній.
+
+        :return: True, якщо список не містить жодного елемента
+        """
+        return self.first is None
+
+    def reset(self):
+        """ Робить перший елемент списку, поточним.
+
+        Переставляє поточний елемент на перший елемент списку
+        Гарантується, що функція не буде викликана, якщо список порожній.
+        """
+        self.curr = self.first
+
+    def next(self):
+        """ Перейти до наступного елемента.
+
+        Робить поточним елементом списку, елемент що йде за поточним.
+        Породжує виключення StopIteration, якщо поточний елемент є останнім у списку.
+        """
+        if self.curr is None or self.curr.next is None:
+            raise StopIteration
+
+
+        self.curr = self.curr.next
+
+    def current(self):
+        """ Повертає навантаження поточного елементу.
+
+        Гарантується, що функція не буде викликана, якщо список порожній.
+        :return: Навантаження поточного елементу
+        """
+        return self.curr.data
+
+    def insert_after(self, item):
+        """ Вставляє новий елемент у список після поточного.
+
+        :param item: елемент, що вставляється у список
+        """
+        newNode = Node(item)
+        if self.empty():  # випадок якщо список порожній
+            self.curr = self.first = newNode
+        else:  # випадок список НЕ порожній
+            newNode.next = self.curr.next
+            self.curr.next = newNode
+###################################################################
+
+
+myList = ClassicList()
+
 
 def init():
     """ Викликається один раз на початку виконання програми. """
-    pass
+    global myList
+    myList = ClassicList()
 
 
 def empty():
@@ -16,7 +80,7 @@ def empty():
 
     :return: True, якщо список не містить жодного елемента
     """
-    return None
+    return myList.empty()
 
 
 def reset():
@@ -25,7 +89,7 @@ def reset():
     Переставляє поточний елемент на перший елемент списку
     Гарантується, що функція не буде викликана, якщо список порожній.
     """
-    pass
+    myList.reset()
 
 
 def next():
@@ -34,7 +98,7 @@ def next():
     Робить поточним елементом списку, елемент що йде за поточним.
     Породжує виключення StopIteration, якщо поточний елемент є останнім у списку.
     """
-    pass
+    myList.next()
 
 
 def current():
@@ -43,7 +107,7 @@ def current():
     Гарантується, що функція не буде викликана, якщо список порожній.
     :return: Навантаження поточного елементу
     """
-    return None
+    return myList.current()
 
 
 def insert_after(item):
@@ -51,4 +115,18 @@ def insert_after(item):
 
     :param item: елемент, що вставляється у список
     """
-    pass
+    myList.insert_after(item)
+
+
+if __name__ == '__main__':
+    myList = ClassicList()
+
+    myList.insert_after(10)
+    myList.insert_after(20)
+    myList.insert_after(30)
+    myList.reset()
+    print(myList.current())
+    myList.next()
+    print(myList.current())
+
+
