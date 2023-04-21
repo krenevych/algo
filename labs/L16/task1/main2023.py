cd125cda2682129e0a0660c86532f982216d074e
Andrii Krenevych <akrenevych@gmail.com>
2023-04-21 10:05:26 +0300

    Solution 1

--- labs/L16/task1/main2023.py
+++ labs/L16/task1/main2023.py
@@ -1,22 +1,50 @@
 
-def isPhoneConsistent(phone):
-    # TODO: implement algorithm
-    print(f"Phone number {phone}")
-    return True
+class PrefixTree:
+    def __init__(self, key):
+        self.key = key
+        self.children = {}  # пари ключ, піддерево
+
+    def addChild(self, tail):
+        if len(tail) == 0:
+            return
+
+        key = tail[0]
+
+        child = self.children.get(key)
+        if child is None:
+            child = PrefixTree(key)
+            self.children[key] = child
+
+        child.addChild(tail[1:])
+
+    def __repr__(self):
+        return f"{self.key}"
+
+def checkPhone(root, phone):
+    if len(phone) == 0:
+        return len(root.children) == 0
+    else:
+        key = phone[0]
+        child = root.children[key]
+        return checkPhone(child, phone[1:])
 
 
-with open("input.txt") as f:
-    testNum = int(f.readline())
-    for i in range(testNum):
 
-        phonesNum = int(f.readline())
-        res = True
-        for ph in range(phonesNum):
-            phone = (f.readline()).strip()
-            if not isPhoneConsistent(phone):
-                res = False
+if __name__ == '__main__':
+    with open("input.txt") as f:
+        testNum = int(f.readline())
+        for i in range(testNum):
+            phones = []
+            phonesNum = int(f.readline())
+            root = PrefixTree("#")
+            for ph in range(phonesNum):
+                phone = (f.readline()).strip()
+                phones.append(phone)
+                root.addChild(phone)
 
-        if res:
-            print("YES")
-        else:
-            print("NO")
+            for phone in phones:
+                if not checkPhone(root, phone):
+                    print("NO")
+                    break
+            else:
+                print("YES")
