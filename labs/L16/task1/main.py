b40ea84f6af54aeffc5cecd96760eda11bd8a8a3
Andrii Krenevych <akrenevych@gmail.com>
2023-04-21 11:13:01 +0300

    Умови до лабораторних 16, 17

--- labs/L16/task1/main.py
+++ labs/L16/task1/main.py
@@ -1,49 +1,17 @@
-class Tree:
-    def __init__(self, key):
-        self.key = key
-        self.isLeaf = False
-        self.children = {}
 
-    def insertNode(self, key):
-        node = Tree(key)
-        self.children[key] = node
+if __name__ == '__main__':
+    with open("input.txt") as f:
+        testNum = int(f.readline())
+        for i in range(testNum):
 
-    def __contains__(self, item):
-        return item in self.children
+            phonesNum = int(f.readline())
+            res = True
+            for ph in range(phonesNum):
+                phone = (f.readline()).strip()
+                print(phone)
+                # TODO: implement algorithm
 
-    def __getitem__(self, item):
-        return self.children[item]
-
-    def __str__(self):
-        return str(self.key) +" -> "+ str(list(self.children.keys()))
-
-    def insert(self, telNumber):
-        cur = self
-
-        if len(telNumber) == 0:
-            return
-
-        childId = telNumber[0]
-        if childId not in self:
-            self.insertNode(childId)
-
-        child = self[childId]
-        rest = telNumber[1:]
-
-        if child.isLeaf or (len(child.children) != 0 and len(rest) == 0):
-            return False
-            
-
-        if len(rest) == 0:
-            child.isLeaf = True
-
-        res = child.insert(rest)
-
-        return res
-
-
-if __name__ == "__main__":
-    tree = Tree("+")
-    print(tree.insert("12"))
-    print(tree.insert("123"))
-    pass
+            if res:
+                print("YES")
+            else:
+                print("NO")
--- labs/L16/task2/main.py
+++ labs/L16/task2/main.py
@@ -1,64 +1,21 @@
-class Tree:
-    def __init__(self, key=None):
-        self.key = key
-        self.children = {}
-        self.parent = None
-
-    def __str__(self):
-        return str(self.key)
-
-    def addChild(self, node):
-        self.children[node.key] = node
-        node.parent = self
-
-    def depth(self):
-        curr = self
-        h = 0
-        while curr != None:
-            h += 1
-            curr = curr.parent
-        return h
-
-
 def add(parent_key, child_key):
-    global tree_nodes
-    child = Tree(child_key)
-    tree_nodes[child_key] = child
-    parent = tree_nodes[parent_key]
-    parent.addChild(child)
+    print(f"ADD: parent_key = {parent_key}, child_key = {child_key}")
+
 
 def lsa(key1, key2):
-    global tree_nodes
-    if key1 not in tree_nodes or key2 not in tree_nodes:
-        return 0
-    node1 = tree_nodes[key1]
-    node2 = tree_nodes[key2]
-    h1 = node1.depth()
-    h2 = node2.depth()
-    while h1 != h2:
-        if h1 > h2:
-            node1 = node1.parent
-            h1 -= 1
-        else:
-            node2 = node2.parent
-            h2 -= 1
+    print(f"GET: key1 = {key1}, key2 = {key2}")
+    # return lsa
 
-    while node1 != node2:
-        node1 = node1.parent
-        node2 = node2.parent
 
-    return node1.key
-
-tree_nodes = {}
-with open("input.txt") as f:
-    tree_nodes[1] = Tree(1)
-    testNum = int(f.readline().strip())
-    for i in range(testNum):
-        line = f.readline().split()
-        command = line[0]
-        key1 = int(line[1])
-        key2 = int(line[2])
-        if command == "ADD":
-            add(key1, key2)
-        elif command == "GET":
-            print(lsa(key1, key2))
+if __name__ == '__main__':
+    with open("input.txt") as f:
+        testNum = int(f.readline().strip())
+        for i in range(testNum):
+            line = f.readline().split()
+            command = line[0]
+            key1 = int(line[1])
+            key2 = int(line[2])
+            if command == "ADD":
+                add(key1, key2)
+            elif command == "GET":
+                print(lsa(key1, key2))
