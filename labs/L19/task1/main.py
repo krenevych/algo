7783209820ff91cb6b296c8fbc66b00f22ea962b
Andrii Krenevych <akrenevych@gmail.com>
2023-04-28 10:33:51 +0300

    Умови до лабораторної 19

--- labs/L19/task1/main.py
+++ labs/L19/task1/main.py
@@ -1,68 +1,22 @@
 class Heap:
     def __init__(self, max_heap_size):
-        self.data = [0] * max_heap_size
-        self.size = 1
+        pass
 
     def insert(self, key):
-        # print("insert")
-        # self.data.append(key)
-        self.data[self.size] = key
-        self.size += 1
-        self.siftUp()
+        print(f"insert {key}")
 
     def extract(self):
-        res = self.data[1]
-        self.data[1] = self.data[self.size - 1]
-        self.size -= 1
-        # self.data.pop()
-        self.siftDown()
-
-        return res
-
-    def siftUp(self):
-        start = self.size - 1
-        while start > 1:
-            parent = start // 2
-            if self.data[start] > self.data[parent]:
-                self.swap(start, parent)
-            else:
-                break
-
-            start = parent
+        print("extract: ", end="")
+        return None
 
 
-    def swap(self, i, j):
-        self.data[i], self.data[j] = self.data[j], self.data[i]
-
-    def siftDown(self):
-        start = 1
-        while 2 * start <= self.size - 1:
-            left = 2 * start
-            right = left + 1
-            maxChild = self.getMaxChild(left, right)
-            if self.data[start] < self.data[maxChild]:
-                self.swap(start, maxChild)
-            else:
-                break
-
-            start = maxChild
-
-    def getMaxChild(self, left, right):
-        if right >= self.size:
-            return left
-        elif self.data[left] > self.data[right]:
-            return left
-        else:
-            return right
-
-
-
-with open("input.txt") as f:
-    N = int(f.readline())
-    heap = Heap(N + 1)
-    for quarry in range(N):
-        command = list(map(int, f.readline().split()))
-        if command[0] == 0:
-            heap.insert(command[1])
-        elif command[0] == 1:
-            print(heap.extract())
+if __name__ == '__main__':
+    with open("input.txt") as f:
+        N = int(f.readline())
+        heap = Heap(N + 1)
+        for quarry in range(N):
+            command = list(map(int, f.readline().split()))
+            if command[0] == 0:
+                heap.insert(command[1])
+            elif command[0] == 1:
+                print(heap.extract())
--- labs/L19/task2/main.py
+++ labs/L19/task2/main.py
@@ -1,38 +1,38 @@
-def siftDown(array, start, size):
-    while 2 * start + 1 < size:
-        left = 2 * start + 1
-        right = left + 1
-
-        if right >= size:
-            maxChild = left
-        elif array[left] > array[right]:
-            maxChild = left
-        else:
-            maxChild = right
-
-        if array[start] < array[maxChild]:
-            array[start], array[maxChild] = array[maxChild], array[start]
+def siftDown(array, cur, size):
+    while 2 * cur + 1 < size:
+        maxChild = getMaxChild(array, cur, size)
+        if array[maxChild] > array[cur]:
+            swap(array, maxChild, cur)
+            cur = maxChild
         else:
             break
 
-        start = maxChild
+
+def getMaxChild(arr, cur, size):
+    left = 2 * cur + 1
+    right = left + 1
+    if right >= size:
+        return left
+
+    return left if arr[left] > arr[right] else right
+
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
 
 
 def heapsort(array):
-    size = len(array)
     # відновлюємо у масиві стуруктуру бінарної купи
-    for i in range(size // 2, -1, -1):
-        siftDown(array, i, size)
+    # TODO:
 
     # виштовхуємо у кінець найбільші елементи купи
-    for range_of_heap in range(size - 1, 0, -1):
-        print(*array)
-        array[0], array[range_of_heap] = array[range_of_heap], array[0]
-        print(*array)
-        siftDown(array, 0, range_of_heap)
+    # TODO:
+
+    pass
 
 
-with open("input.txt") as f:
-    array = list(map(int, f.readline().split()))
-    heapsort(array)
-    print(*array)
+if __name__ == '__main__':
+    with open("input.txt") as f:
+        array = list(map(int, f.readline().split()))
+        heapsort(array)
+        print(*array)
