11d0cb8d2cfeda1f651d356e72827df821a9d24b
Andrii Krenevych <akrenevych@gmail.com>
2023-05-05 09:55:53 +0300

    solution for task 21.3

--- labs/L21/t3/main.py
+++ labs/L21/t3/main.py
@@ -4,11 +4,21 @@ if __name__ == '__main__':
         n = int(infile.readline())
         k = int(infile.readline())
 
+        graph = []
+        for i in range(n + 1):
+            graph.append([])
+
         for i in range(k):
             inp = list(map(int, infile.readline().split()))
             if inp[0] == 1:
                 # Add edge
-                print("Add edge:", inp[1], inp[2])
+                # print("Add edge:", inp[1], inp[2])
+                graph[inp[1]].append(inp[2])
+                graph[inp[2]].append(inp[1])
             elif inp[0] == 2:
                 # Vertex
-                print(f"Neighbours {inp[1]}: ")
+                # print(f"Neighbours {inp[1]}: ")
+                print(*graph[inp[1]])
+
+        # for row in graph:
+        #     print(*row)
