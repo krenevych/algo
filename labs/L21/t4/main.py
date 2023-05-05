520f0f23b0297cfe48cf639471f87088a51706aa
Andrii Krenevych <akrenevych@gmail.com>
2023-05-05 09:58:26 +0300

    task for task 21.4

--- labs/L21/t4/main.py
+++ labs/L21/t4/main.py
@@ -1,12 +1,9 @@
+if __name__ == '__main__':
 
-with open("input.txt") as infile:
-    n = int(infile.readline())
-    edges = 0  # кількість ребер
-    for i in range(n):
-        row = list(map(int, infile.readline().split()))
-        for j in range(n):
-            if row[j] > 0:  # знайшли ребро
-                edges += 1
+    with open("input.txt") as infile:
+        n = int(infile.readline())
 
-    print(edges)
+        for i in range(n):
+            row = list(map(int, infile.readline().split()))
+            print(*row)
 
