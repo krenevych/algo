64de0d6af0d7565f77d64b0da14a64ead8ea8f93
Andrii Krenevych <akrenevych@gmail.com>
2023-05-05 09:14:55 +0300

    розв'язок 1

--- labs/L21/t1/main.py
+++ labs/L21/t1/main.py
@@ -4,7 +4,9 @@ if __name__ == '__main__':
 
     with open("input.txt") as infile:
         n = int(infile.readline())
+
         for i in range(n):
             row = list(map(int, infile.readline().split()))
-            print(row)
-
+            for j in range(i, n):
+                if row[j] > 0:
+                    print(i + 1, j + 1)
