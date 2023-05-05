de8d776cbce988d9247578dadeb4d95af7e13d77
Andrii Krenevych <akrenevych@gmail.com>
2023-05-05 10:30:10 +0300

    solution 4

--- labs/L21/t4/main.py
+++ labs/L21/t4/main.py
@@ -2,8 +2,13 @@ if __name__ == '__main__':
 
     with open("input.txt") as infile:
         n = int(infile.readline())
-
+        edges = 0
         for i in range(n):
             row = list(map(int, infile.readline().split()))
-            print(*row)
+            # print(*row)
+            for j in range(n):
+                if row[j] > 0:
+                    edges += 1
+
+        print(edges)
 
