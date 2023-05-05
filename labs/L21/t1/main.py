f848cc6c327a924e515d5cb2c891e171e417c52d
Andrii Krenevych <akrenevych@gmail.com>
2023-05-05 08:55:38 +0300

    Read data

--- labs/L21/t1/main.py
+++ labs/L21/t1/main.py
@@ -1,10 +1,10 @@
 
-with open("input.txt") as infile:
-    n = int(infile.readline())
-    graph = []
-    for i in range(n):
-        row = list(map(int, infile.readline().split()))
-        for j in range(i, n):
-            if row[j] > 0:
-                print(i+1, j+1)
+
+if __name__ == '__main__':
+
+    with open("input.txt") as infile:
+        n = int(infile.readline())
+        for i in range(n):
+            row = list(map(int, infile.readline().split()))
+            print(row)
 
