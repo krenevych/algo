913f8db1d964964a3b97b13e87133e131d86f765
Andrii Krenevych <akrenevych@gmail.com>
2023-05-05 09:32:50 +0300

    input for task 21.3

--- labs/L21/t3/main.py
+++ labs/L21/t3/main.py
@@ -1,19 +1,14 @@
-with open("input.txt") as infile:
-    n = int(infile.readline())
-    k = int(infile.readline())
-    graph = []
-    for i in range(n):
-        graph.append([])
+if __name__ == '__main__':
 
-    for i in range(k):
-        inp = list(map(int, infile.readline().split()))
-        if inp[0] == 1:
-            # Add edge
-            source = inp[1]
-            dest = inp[2]
-            graph[source - 1].append(dest)
-            graph[dest - 1].append(source)
-        elif inp[0] == 2:
-            # Vertex
-            vert = inp[1] - 1
-            print(*graph[vert])
+    with open("input.txt") as infile:
+        n = int(infile.readline())
+        k = int(infile.readline())
+
+        for i in range(k):
+            inp = list(map(int, infile.readline().split()))
+            if inp[0] == 1:
+                # Add edge
+                print("Add edge:", inp[1], inp[2])
+            elif inp[0] == 2:
+                # Vertex
+                print(f"Neighbours {inp[1]}: ")
