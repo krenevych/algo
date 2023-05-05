68a8b3b37be17752564175004cadf844680e84c4
Andrii Krenevych <akrenevych@gmail.com>
2023-05-05 09:24:30 +0300

    Solution 21.2

--- labs/L21/t2/main.py
+++ labs/L21/t2/main.py
@@ -1,7 +1,17 @@
 if __name__ == '__main__':
 
+    graph = []
     with open("input.txt") as infile:
         n = int(infile.readline())
         for i in range(n):
             inp = list(map(int, infile.readline().split()))
-            print(inp)
+            # print(inp)
+            row = [0] * n
+            for j in inp[1:]:
+                row[j-1] = 1
+
+            # print(*row)
+            graph.append(row)
+
+        for row in graph:
+            print(*row)
