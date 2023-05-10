6d6636cbe196a34ab207e0264c69872107ca5636
Andrii Krenevych <akrenevych@gmail.com>
2023-05-10 11:05:21 +0300

    Заготовки до лабораторної 23

--- labs/L23/t1/main.py
+++ labs/L23/t1/main.py
@@ -1,23 +1,14 @@
-
-def dfs(g, visited, start):
-    n = len(g)
-    for neighbour in range(n):
-        if g[start][neighbour] != 0 and neighbour not in visited:
-            visited.add(neighbour)
-            dfs(g, visited, neighbour)
-
-g = []
+# граф реалізовано через матрицю суміжності
+graph = []
 with open("input.txt") as input_file:
     f_line = input_file.readline()
     n, s = [int(el) for el in f_line.split()]
     for i in range(n):
         f_line = input_file.readline()
         row = [int(el) for el in f_line.split()]
-        g.append(row)
+        graph.append(row)
 
-
-visited = set()
-dfs(g, visited, s - 1)
-print(len(visited))
-
-    
+# for debug - remove before submit
+print(f"vertices = {n}, selected vertex = {s}")
+for row in graph:
+    print(*row)
--- labs/L23/t2/main.py
+++ labs/L23/t2/main.py
@@ -1,29 +1,16 @@
-
-def dfs(g, visited, start):
-    for neighbour in g[start]:
-        if neighbour not in visited:
-            visited.add(neighbour)
-            dfs(g, visited, neighbour)
-
-g = []
 with open("input.txt") as input_file:
     f_line = input_file.readline()
     n, m = [int(el) for el in f_line.split()]
-    for i in range(n + 1):
-        neigbours = []
-        g.append(neigbours)
-
+    # граф реалізовано через список суміжності
+    graph = {el: set() for el in range(1, n + 1)}
     for i in range(m):
         f_line = input_file.readline()
-        u, v = [int(el) for el in f_line.split()]
-        g[u].append(v)
-        g[v].append(u)
+        cur, neig = [int(el) for el in f_line.split()]
+        graph[cur].add(neig)
+        graph[neig].add(cur)
 
 
-# print(g)
-
-visited = set()
-dfs(g, visited, 1)
-print("YES" if len(visited) == n else "NO")
-
-
+# for debug - remove before submit
+print(f"vertices = {n}, edges = {m}")
+for v in graph:
+    print(f"{v}: , {graph[v]}")
