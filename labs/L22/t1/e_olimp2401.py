9287b2228f9277b270282c8a937e9717008f45b5
Andrii Krenevych <akrenevych@gmail.com>
2023-05-10 09:21:58 +0300

    solution for task 22.1

--- labs/L22/t1/e_olimp2401.py
+++ labs/L22/t1/e_olimp2401.py
@@ -9,7 +9,47 @@ with open("input.txt") as input_file:
         row = [int(el) for el in f_line.split()]
         g.append(row)
 
-print(f"start = {s}")
-print(f"finish = {f}")
-for row in g:
-    print(*row)
+# print(f"start = {s}")
+# print(f"finish = {f}")
+# for row in g:
+#     print(*row)
+
+class Queue:
+    def __init__(self, size):
+        self.q = [0] * size
+        self.b = 0
+        self.e = 0
+
+    def enque(self, item):
+        self.q[self.e] = item
+        self.e += 1
+
+    def deque(self):
+        item = self.q[self.b]
+        self.b += 1
+        return item
+
+    def empty(self):
+        return self.b == self.e
+
+def bfs():
+    q = Queue(120)
+    q.enque(s-1)
+
+    visited = {s - 1: 0}
+    while not q.empty():
+        cur = q.deque()
+        # print(cur + 1)
+        for v in range(n):
+            if g[cur][v] == 1 and v not in visited:
+                q.enque(v)
+                visited[v] = visited[cur] + 1
+
+    if f - 1 in visited:
+        return visited[f - 1]
+    else:
+        return 0
+
+
+dist = bfs()
+print(dist)
