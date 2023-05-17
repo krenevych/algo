ddc444874bf2636a30ce9c33314837f6aec42b5c
Andrii Krenevych <akrenevych@gmail.com>
2023-05-17 08:56:19 +0300

    Умова 24 лаб

--- labs/L24/t1/main.py
+++ labs/L24/t1/main.py
@@ -1,8 +1,11 @@
 
 
 with open("input.txt") as input_file:
-    n = int(input_file.readline().strip())
+    n = int(input_file.readline())
     print(n)
     for i in range(n):
         row = input_file.readline().strip()
         print(row)
+
+    start_row, start_col = map(int, input_file.readline().split())
+    print(start_row, start_col)
