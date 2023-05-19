238b11ac1d1500d078b9ff2b7c91628064242f30
Andrii Krenevych <akrenevych@gmail.com>
2023-05-19 10:19:24 +0300

    Лабораторна 25

--- labs/L25/task1/user.py
+++ labs/L25/task1/user.py
@@ -2,7 +2,20 @@
 Нехай задано орієнтований зважений граф.
 Знайдіть довжину найкоротшого шляху між двома заданими вузлами цього графа
 """
+graph = []
+INF = 100500
 
+class Vertex:
+    def __init__(self, key):
+        self.key = key
+        self.neigbours = {}
+        self.distance = INF
+
+    def __str__(self):
+        return f"{self.key} , dist({self.distance}) : {self.neigbours}"
+
+    def __repr__(self):
+        return str(self)
 
 def init(vertices, edges):
     """ Ініціалізація графа.
@@ -11,6 +24,10 @@ def init(vertices, edges):
     @param vertices: кількість вершин графа
     @param edges:  кількість ребер графа
     """
+    global graph
+    graph = []
+    for v in range(vertices):
+        graph.append(Vertex(v))
     pass
 
 
@@ -21,8 +38,9 @@ def addEdge(source, destination, weight):
     @param destination: вершина у яку входить ребро
     @param weight: вага ребра
     """
+    # print("addEdge: ", source, destination, weight)
+    graph[source].neigbours[destination] = weight
     pass
-
 
 def findDistance(start, end):
     """ Знаходить довжину найкоротшого шляху, між двома заданими вершинами графа
@@ -31,5 +49,16 @@ def findDistance(start, end):
     @param end: кінцева вершина
     @return: Довжину найкоротшого шляху або -1 якщо шляху між вершинами не існує.
     """
+    global graph
+    vertex = graph[start]
+    vertex.distance = 0
 
-    return -1
+    for i in range(len(graph) - 1):
+        for vertex in graph:
+            for neig, weight in vertex.neigbours.items():
+                new_dist = vertex.distance + weight
+                if new_dist < graph[neig].distance:
+                    graph[neig].distance = new_dist
+                pass
+    dist = graph[end].distance
+    return dist if dist < INF else -1
--- labs/L25/task2/user.py
+++ labs/L25/task2/user.py
@@ -2,7 +2,23 @@
 Нехай задано орієнтований зважений граф.
 Знайдіть найкоротший шляху між двома заданими вузлами цього графа
 """
+from labs.L25.task2.PriorityQueue import PriorityQueue
 
+graph = []
+INF = 100500
+
+class Vertex:
+    def __init__(self, key):
+        self.key = key
+        self.neigbours = {}
+        self.distance = INF
+        self.source = None
+
+    def __str__(self):
+        return f"{self.key} , dist({self.distance}) : {self.neigbours}"
+
+    def __repr__(self):
+        return str(self)
 
 def init(vertices, edges):
     """ Ініціалізація графа.
@@ -11,7 +27,10 @@ def init(vertices, edges):
     @param vertices: кількість вершин графа
     @param edges:  кількість ребер графа
     """
-    pass
+    global graph
+    graph = []
+    for v in range(vertices):
+        graph.append(Vertex(v))
 
 
 def addEdge(source, destination, weight):
@@ -21,7 +40,7 @@ def addEdge(source, destination, weight):
     @param destination: вершина у яку входить ребро
     @param weight: вага ребра
     """
-    pass
+    graph[source].neigbours[destination] = weight
 
 
 def getWay(start, end):
@@ -31,5 +50,31 @@ def getWay(start, end):
     @param end: кінцева вершина
     @return: список вершин шляху або порожній список, якщо шляху між вершинами не існує.
     """
+    global graph
+    vertex = graph[start]
+    vertex.distance = 0
 
-    return []
+    pq = PriorityQueue()
+    for v in graph:
+        pq.insert(v.key, v.distance)
+
+    while not pq.empty():
+        vertex_key = pq.extractMinimum()
+        vertex = graph[vertex_key]
+        for neig, weight in vertex.neigbours.items():
+            new_dist = vertex.distance + weight
+            if new_dist < graph[neig].distance:
+                graph[neig].distance = new_dist
+                graph[neig].source = vertex
+                pq.updatePriority(neig, graph[neig].distance)
+
+    if graph[end].source == None:
+        return []
+
+    way = []
+    cur_vertex = graph[end]
+    while cur_vertex is not None:
+        way.append(cur_vertex.key)
+        cur_vertex = cur_vertex.source
+    way = way[::-1]
+    return way
