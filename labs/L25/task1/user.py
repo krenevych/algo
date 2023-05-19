883c62f7846dde84204ea09a31fa1ce18ad2a98d
Andrii Krenevych <akrenevych@gmail.com>
2023-05-19 08:48:34 +0300

    Умови 25

--- labs/L25/task1/user.py
+++ labs/L25/task1/user.py
@@ -2,20 +2,6 @@
 Нехай задано орієнтований зважений граф.
 Знайдіть довжину найкоротшого шляху між двома заданими вузлами цього графа
 """
-INF = 100000000
-
-
-class Vertex:
-    def __init__(self, key):
-        self.key = key
-        self.neighbour = {}  # список (словник ключ_вершини: вага ребра) сусідів
-        self.distance = INF
-
-    def __str__(self):
-        return str(self.key) + ":" + str(*self.neighbour.keys())
-
-
-graph = []  # граф, як список вершин
 
 
 def init(vertices, edges):
@@ -25,10 +11,7 @@ def init(vertices, edges):
     @param vertices: кількість вершин графа
     @param edges:  кількість ребер графа
     """
-    global graph
-    graph = []  # Створюємо новий граф
-    for i in range(vertices):
-        graph.append(Vertex(i))
+    pass
 
 
 def addEdge(source, destination, weight):
@@ -38,9 +21,7 @@ def addEdge(source, destination, weight):
     @param destination: вершина у яку входить ребро
     @param weight: вага ребра
     """
-    global graph
-    graph[source].neighbour[destination] = weight
-    return True
+    pass
 
 
 def findDistance(start, end):
@@ -50,17 +31,5 @@ def findDistance(start, end):
     @param end: кінцева вершина
     @return: Довжину найкоротшого шляху або -1 якщо шляху між вершинами не існує.
     """
-    global graph
-    for v in graph:
-        v.distance = INF
 
-    graph[start].distance = 0
-
-    for i in range(len(graph) - 1):
-        for v in graph:
-            for neigb, weight in v.neighbour.items():
-                new_dist = v.distance + weight
-                if new_dist < graph[neigb].distance:
-                    graph[neigb].distance = new_dist
-
-    return graph[end].distance if graph[end].distance < INF else -1
+    return -1
--- labs/L25/task2/user.py
+++ labs/L25/task2/user.py
@@ -2,23 +2,7 @@
 Нехай задано орієнтований зважений граф.
 Знайдіть найкоротший шляху між двома заданими вузлами цього графа
 """
-from labs.L25.task2.PriorityQueue import PriorityQueue
 
-INF = 100000000
-
-
-class Vertex:
-    def __init__(self, key):
-        self.key = key
-        self.neighbour = {}  # список (словник ключ_вершини: вага ребра) сусідів
-        self.distance = INF
-        self.source = None  # використовується для побудови шляху - вершина з якої ми прийшли у поточну вершину
-
-    def __str__(self):
-        return str(self.key) + ":" + str(*self.neighbour.keys())
-
-
-graph = []  # граф, як список вершин
 
 def init(vertices, edges):
     """ Ініціалізація графа.
@@ -27,10 +11,7 @@ def init(vertices, edges):
     @param vertices: кількість вершин графа
     @param edges:  кількість ребер графа
     """
-    global graph
-    graph = []
-    for i in range(vertices):
-        graph.append(Vertex(i))
+    pass
 
 
 def addEdge(source, destination, weight):
@@ -40,8 +21,7 @@ def addEdge(source, destination, weight):
     @param destination: вершина у яку входить ребро
     @param weight: вага ребра
     """
-    global graph
-    graph[source].neighbour[destination] = weight
+    pass
 
 
 def getWay(start, end):
@@ -51,35 +31,5 @@ def getWay(start, end):
     @param end: кінцева вершина
     @return: список вершин шляху або порожній список, якщо шляху між вершинами не існує.
     """
-    global graph
-    for v in graph:
-        v.distance = INF
-        v.source = None
 
-    graph[start].distance = 0
-    pq = PriorityQueue()
-    for key_ver in range(len(graph)):
-        pq.insert(key_ver, graph[key_ver].distance)
-
-    while not pq.empty():
-        vert = pq.extractMinimum()
-
-        for neigb, weight in graph[vert].neighbour.items():
-            new_dist = graph[vert].distance + weight
-            if neigb in pq and new_dist < graph[neigb].distance:
-                graph[neigb].distance = new_dist
-                graph[neigb].source = vert
-                pq.updatePriority(neigb, graph[neigb].distance)
-
-    if graph[end].distance == INF:
-        return []
-
-    curr = end
-    way = []
-    while True:
-        way.append(curr)
-        if curr == start:
-            break
-        curr = graph[curr].source
-
-    return way[::-1]
+    return []
